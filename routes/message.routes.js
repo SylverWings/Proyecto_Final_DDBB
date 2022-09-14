0dceeb8a0c55013082ff960204f809a43d7114dc
Lionel <Lionel5garcia@gmail.com>
2022-09-14 21:17:59 +0200

    feat: Message controller created

--- routes/message.routes.js
+++ routes/message.routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const messageController = require("../controllers/messageController");
+const messageController = require("../controllers/MessageController");
 const isSuperAdmin = require('../middlewares/isSuperAdmin');
 const verifyToken = require('../middlewares/verifyToken');
 
